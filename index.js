66f231ee40dea488852e6e0b2cd1dbbc8aedc089
Singh <ar-virendra.singh@rakuten.com>
2021-01-19 00:05:07 +0530

    Added user and task routes

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var express=require('express');
 var bodyparser=require('body-parser');
 var cors=require('cors');
 
+var userRoutes=require('./routes/user');
+var taskRoutes=require('./routes/user');
+
 const app=express();
 const port=5000;
 
@@ -13,6 +16,9 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
 mongoose.Promise=global.Promise;
+
+app.use('/user',userRoutes);
+app.use('/task',taskRoutes);
 
 mongoose.connect("mongodb+srv://5Simran_Singh:Simran5@clusteras.zogxl.mongodb.net/ProTask?retryWrites=true&w=majority",{useNewUrlParser:true})
 .then(()=>{
